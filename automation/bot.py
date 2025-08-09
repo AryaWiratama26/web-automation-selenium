cdc65a288b659356c6be0f222047cd58bf95ef81
Arya Wiratama <aryawiratama2401@gmail.com>
2025-08-09 21:45:52 +0700

    udpate: login to website

---
+++ automation/bot.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from datetime import date
+
+CHROME_DRIVER_PATH = "/usr/local/bin/chromedriver"
+
+OP = webdriver.ChromeOptions()
+# OP.add_argument('--headless')  
+
+service = Service(executable_path=CHROME_DRIVER_PATH)
+
+DRIVER = webdriver.Chrome(service=service, options=OP)
+
+
+class AutomationWeb:
+    def __init__(self, EMAIL, PASSWORD, LINK_LOGIN):
+        self.email = EMAIL
+        self.password = PASSWORD
+        self.link_login = LINK_LOGIN
+        
+    def login(self):
+        try:
+            DRIVER.get(self.link_login)
+            
+            time.sleep(5)
+            username = DRIVER.find_element(By.CSS_SELECTOR, value="input[name='username']")
+            password = DRIVER.find_element(By.CSS_SELECTOR, value="input[name='password']")
+            continue_btn = DRIVER.find_element(By.ID, value="login-submit")
+
+            # send keys email
+            username.send_keys(self.email)
+            time.sleep(2)
+
+            # klik button
+            continue_btn.click()
+            time.sleep(2)
+
+            # send keys password
+            password.send_keys(self.password)
+            time.sleep(3)
+
+            # klik button
+            continue_btn.click()
+            time.sleep(10)
+
+            
+        except Exception as e:
+            print(f"kesalahan {e}")
+            DRIVER.close()
+            
+            
+    
---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+from automation import AutomationWeb
+
+load_dotenv()
+EMAIL_AKUN = os.getenv("EMAIL")
+PASSWORD_AKUN = os.getenv("PASSWORD")
+LINK_WEBSITE = os.getenv("LINK_LOGIN")
+
+# print(f"{EMAIL_AKUN}, {PASSWORD_AKUN}, {LINK_WEBSITE}")
+
+if __name__ == "__main__":
+    bot_satu = AutomationWeb(EMAIL=EMAIL_AKUN, PASSWORD=PASSWORD_AKUN, LINK_LOGIN=LINK_WEBSITE)
+    bot_satu.login()
